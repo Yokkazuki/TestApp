649b8af725d50b90870fa295bdd0628043b6edf6
Pastel Link <sma990404@gmail.com>
2022-03-17 12:56:02 +0800

    updated navbar color theme

--- src/Components/Pages/Navigation.js
+++ src/Components/Pages/Navigation.js
@@ -1,46 +1,50 @@
-import React from "react"
-import "./Navigation.css"
-import { FaHome } from "react-icons/fa"
-import { Button, FormControl, InputGroup } from "react-bootstrap"
-import { linkItems } from "../../Data/navigationLinkList"
-import DarkModeButton from "../../Theme/ThemeButton"
+import React, { useCallback, useContext } from "react";
+import "./Navigation.scss";
+import { FaHome } from "react-icons/fa";
+import { Button, FormControl, InputGroup } from "react-bootstrap";
+import { linkItems } from "../../Data/navigationLinkList";
+import DarkModeButton from "../../Theme/ThemeButton";
+import { ThemeContext } from "../../Theme/ThemeContext";
 
 export default function Navigation() {
+  const { darkMode } = useContext(ThemeContext);
   return (
-    <div className="navbar">
-      {/* left */}
-      <div className="nav-left">
-        <div className="brand">
-          <a href="/#">
-            <FaHome className="nav-icon" />
-          </a>
+    <div id={darkMode ? "theme-dark" : "theme-light"}>
+      <div className="navbar">
+        {/* left */}
+        <div className="nav-left">
+          <div className="brand">
+            <a href="/#">
+              <FaHome className="nav-icon" />
+            </a>
+          </div>
         </div>
-      </div>
 
-      {/* mid */}
-      <div className="nav-mid">
-        <InputGroup>
-          <FormControl placeholder="Type something and search" />
-          <Button className="nav-searchButton">Search</Button>
-        </InputGroup>
-      </div>
+        {/* mid */}
+        <div className="nav-mid">
+          <InputGroup>
+            <FormControl placeholder="Type something and search" />
+            <Button className="nav-searchButton">Search</Button>
+          </InputGroup>
+        </div>
 
-      {/* right */}
-      <div className="nav-right">
-        <div className="linkList">
-          {linkItems.length > 0 &&
-            linkItems.map((item) => {
-              return (
-                <a className="linkItem" href={item.link} key={item.title}>
-                  {item.title}
-                </a>
-              )
-            })}
-          <div className="linkItem">
-            <DarkModeButton />
+        {/* right */}
+        <div className="nav-right">
+          <div className="linkList">
+            {linkItems.length > 0 &&
+              linkItems.map((item) => {
+                return (
+                  <a className="linkItem" href={item.link} key={item.title}>
+                    {item.title}
+                  </a>
+                );
+              })}
+            <div className="linkItem">
+              <DarkModeButton />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }

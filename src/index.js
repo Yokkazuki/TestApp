dd215341168968d1d14c9885dbd9bbe809fda43d
Pastel Link <sma990404@gmail.com>
2022-03-23 10:19:17 +0800

    dev

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "./Theme/ThemeContext";
 
+
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>

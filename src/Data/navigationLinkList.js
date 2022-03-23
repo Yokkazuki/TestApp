46d1d7acb06e4566c3167cb4c4a6c66b32fe5ea7
Pastel Link <sma990404@gmail.com>
2022-03-23 10:36:24 +0800

    dev

--- src/Data/navigationLinkList.js
+++ src/Data/navigationLinkList.js
@@ -1,25 +1,25 @@
 export const linkItems = [
   {
     title: "Home",
-    link: "/",
+    link: "/TestApp",
     dropdown: false,
     dropdownItem: [],
   },
   {
     title: "Page1",
-    link: "/#/Page1",
+    link: "/TestApp/#/Page1",
     dropdown: false,
     dropdownItem: [],
   },
   {
     title: "Page2",
-    link: "/#/Page2",
+    link: "/TestApp/#/Page2",
     dropdown: false,
     dropdownItem: [],
   },
   {
     title: "page3",
-    link: "/#/Page3",
+    link: "/TestApp/#/Page3",
     dropdown: false,
     dropdownItem: [],
   },
